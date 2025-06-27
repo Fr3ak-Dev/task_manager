da332a04d4c51577a6b57eca1ee6709ba4048b3c
gustavo <kg.2k.pl@gmail.com>
2025-06-26 20:46:55 -0400

    feat(routes): add route to fetch a specific task by ID for a project

--- server/src/controllers/TaskController.ts
+++ server/src/controllers/TaskController.ts
@@ -11,16 +11,32 @@ export class TaskController {
             await Promise.allSettled([task.save(), req.project.save()])
             res.send("Task created")
         } catch (error) {
-            res.status(500).json({error: 'Internal server error'})
+            res.status(500).json({ error: 'Internal server error' })
         }
     }
 
     static getProjectTasks = async (req: Request, res: Response) => {
         try {
-            const tasks = await Task.find({project: req.project.id}).populate('project')
+            const tasks = await Task.find({ project: req.project.id }).populate('project')
             res.json(tasks)
         } catch (error) {
-            res.status(500).json({error: 'Internal server error'})
+            res.status(500).json({ error: 'Internal server error' })
+        }
+    }
+
+    static getTaskById = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params
+            const task = await Task.findById(taskId)
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' })
+            }
+            if (task.project.toString() !== req.project.id) {
+                return res.status(400).json({ error: 'Action not allowed' })
+            }
+            res.json(task)
+        } catch (error) {
+            res.status(500).json({ error: 'Internal server error' })
         }
     }
 }
