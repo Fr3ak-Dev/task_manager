3296febc18e5b582f094b4cad6d4f36e110cb4b3
gustavo <kg.2k.pl@gmail.com>
2025-06-26 20:09:09 -0400

    feat(routes): add route to fetch tasks for a specific project

--- server/src/controllers/TaskController.ts
+++ server/src/controllers/TaskController.ts
@@ -11,7 +11,16 @@ export class TaskController {
             await Promise.allSettled([task.save(), req.project.save()])
             res.send("Task created")
         } catch (error) {
-            console.log(error)
+            res.status(500).json({error: 'Internal server error'})
+        }
+    }
+
+    static getProjectTasks = async (req: Request, res: Response) => {
+        try {
+            const tasks = await Task.find({project: req.project.id})
+            res.json(tasks)
+        } catch (error) {
+            res.status(500).json({error: 'Internal server error'})
         }
     }
 }
