c4b4af403c58674e5a529859f8d8103c6bbe471b
gustavo <kg.2k.pl@gmail.com>
2025-07-01 21:26:30 -0400

    feat(nav): add NavMenu component for navigation and integrate into AppLayout

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Logo from "@/components/Logo";
+import NavMenu from "@/components/NavMenu";
 
 export default function AppLayout() {
     return (
         <>
-            <header className="bg-gray-800 py-5">
+            <header className="bg-gray-800 py-5 px-30">
                 <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
                     <div className="w-64">
                         <Logo />
                     </div>
+                    <NavMenu />
                 </div>
-            </header> 
-            <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+            </header>
+            <section className="max-w-screen-2xl mx-auto mt-10 p-5 px-30">
                 <Outlet />
             </section>
             <footer className="py-5">
