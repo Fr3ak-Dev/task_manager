9ce5ffa2bdfd51d914f74d6b76aa008e0ec2e2ec
gustavo <kg.2k.pl@gmail.com>
2025-07-01 23:54:36 -0400

    feat: add CreateProjectView and integrate with routing from DashboardView

--- client/src/views/DashboardView.tsx
+++ client/src/views/DashboardView.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function DashboardView() {
   return (
-    <div>DashboardView</div>
+    <>
+      <h1 className="text-5xl font-black">Mis Proyectos</h1>
+      <p className="text-2xl font-light">Maneja y administra tus proyectos</p>
+      <nav className="my-5">
+        <Link
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          to="/projects/create"
+        >
+          Nuevo Proyecto
+        </Link>
+      </nav>
+    </>
   )
 }
