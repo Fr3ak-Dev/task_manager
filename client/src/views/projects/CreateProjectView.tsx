ef7ff17cedb2ee71717dfa165218845f8bb75203
gustavo <kg.2k.pl@gmail.com>
2025-07-07 19:25:39 -0400

    feat: add Zod for schema validation and refactor ProjectForm to use ProjectFormData type

--- client/src/views/projects/CreateProjectView.tsx
+++ client/src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,11 @@
 import ProjectForm from "@/components/projects/ProjectForm";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import type { ProjectFormData } from "types";
 
 export default function CreateProjectView() {
 
-    const initialValues = {
+    const initialValues : ProjectFormData = {
         projectName: '',
         clientName: '',
         description: '',
@@ -18,7 +19,7 @@ export default function CreateProjectView() {
      */
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-    const handleForm = (data) => {
+    const handleForm = (data : ProjectFormData) => {
         console.log(data)
     }
 
