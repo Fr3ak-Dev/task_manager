4ad4c56a88d0a0d2f326f40b62eb1cb12586ab25
gustavo <kg.2k.pl@gmail.com>
2025-07-07 19:57:46 -0400

    feat(CreateProjectView): implement project creation functionality and update imports

--- client/src/views/projects/CreateProjectView.tsx
+++ client/src/views/projects/CreateProjectView.tsx
@@ -1,7 +1,8 @@
-import ProjectForm from "@/components/projects/ProjectForm";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import type { ProjectFormData } from "types";
+import ProjectForm from "@/components/projects/ProjectForm"
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
+import { ProjectFormData } from "@/types/index"
+import { createProject } from "@/api/ProjectAPI"
 
 export default function CreateProjectView() {
 
@@ -20,7 +21,7 @@ export default function CreateProjectView() {
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
     const handleForm = (data : ProjectFormData) => {
-        console.log(data)
+        createProject(data)
     }
 
     return (
