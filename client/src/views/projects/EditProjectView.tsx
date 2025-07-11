ab39117d9ca313b355c555444fc4dfb539d9c803
gustavo <kg.2k.pl@gmail.com>
2025-07-10 21:06:42 -0400

    feat(EditProjectView): implement project fetching logic using getProjectById with react-query

--- client/src/views/projects/EditProjectView.tsx
+++ client/src/views/projects/EditProjectView.tsx
@@ -1,9 +1,19 @@
+import { getProjectById } from "@/api/ProjectAPI"
+import { useQuery } from "@tanstack/react-query"
 import { useParams } from "react-router-dom"
 
 export default function EditProjectView() {
     const params = useParams()
     const projectId = params.projectId!
-    console.log(projectId)
+    
+    const { data, isLoading, error, isError} = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+    })
+
+    console.log(data)
+
     return (
         <div>EditProjectView</div>
     )
