10986cd386dd10bc75a02003d6fea68058ded959
gustavo <kg.2k.pl@gmail.com>
2025-07-16 22:25:51 -0400

    feat(TaskModalDetails): enhance task detail display with fetched data

--- client/src/components/tasks/TaskModalDetails.tsx
+++ client/src/components/tasks/TaskModalDetails.tsx
@@ -31,7 +31,7 @@ export default function TaskModalDetails() {
 
     console.log(data)
 
-    return (
+    if(data) return (
         <>
             <Transition appear show={show} as={Fragment}>
                 <Dialog as="div" className="relative z-10" onClose={() => navigate(location.pathname, { replace: true })}>
@@ -64,9 +64,9 @@ export default function TaskModalDetails() {
                                     <Dialog.Title
                                         as="h3"
                                         className="font-black text-4xl text-slate-600 my-5"
-                                    >Titulo aquí
+                                    >{data.name}
                                     </Dialog.Title>
-                                    <p className='text-lg text-slate-500 mb-2'>Descripción:</p>
+                                    <p className='text-lg text-slate-500 mb-2'>Descripción: {data.description}</p>
                                     <div className='my-5 space-y-3'>
                                         <label className='font-bold'>Estado Actual:</label>
                                     </div>
