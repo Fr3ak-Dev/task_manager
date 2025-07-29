82bb39bee90e5d64c2af360a91c8bbff9d3f3675
gustavo <kg.2k.pl@gmail.com>
2025-07-29 06:40:01 -0400

    feat(auth): add endpoint to reset password and send token by email

--- server/src/emails/AuthEmail.ts
+++ server/src/emails/AuthEmail.ts
@@ -23,4 +23,21 @@ export class AuthEmail {
 
         console.log('Mensaje enviado', info.messageId)
     }
+
+    static sendPasswordResetToken = async (user : IEmail) => {
+        const info = await transporter.sendMail({
+            from: 'Tarea <admin@gmail.com>',
+            to: user.email,
+            subject: 'Reestablece tu password',
+            text: 'Reestablece tu password',
+            html: `<p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
+                <p>Visita el siguiente enlace:</p>
+                <a href="${process.env.FRONT_URL}/auth/new-password">Reestablecer Password</a>    
+                <p>E ingresa el código: <b>${user.token}</b></p>
+                <p>Este token expira en 10 minutos</p>
+            `
+        })
+
+        console.log('Mensaje enviado', info.messageId)
+    }
 }
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -37,4 +37,10 @@ router.post('/request-code',
     AuthController.requestConfirmationCode
 )
 
+router.post('/forgot-password',
+    body('email').isEmail().withMessage('E-mail no válido'),
+    handleInputErrors,
+    AuthController.forgotPassword
+)
+
 export default router
