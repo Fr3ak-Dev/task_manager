acdb7262b5aac2912b331910c8766bd1659b65db
gustavo <kg.2k.pl@gmail.com>
2025-07-30 06:38:05 -0400

    feat(auth): add endpoint to validate token and allow password reset

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -7,8 +7,8 @@ const router = Router()
 
 router.post('/create-account',
     body('name').notEmpty().withMessage('El nombre es requerido'),
-    body('password').isLength({min:8}).withMessage('El password es muy corto, mínimo 8 caracteres'),
-    body('password_confirmation').custom((value, {req}) => {
+    body('password').isLength({ min: 8 }).withMessage('El password es muy corto, mínimo 8 caracteres'),
+    body('password_confirmation').custom((value, { req }) => {
         if (value !== req.body.password) {
             throw new Error('Los password no coinciden')
         }
@@ -41,6 +41,12 @@ router.post('/forgot-password',
     body('email').isEmail().withMessage('E-mail no válido'),
     handleInputErrors,
     AuthController.forgotPassword
+)
+
+router.post('/validate-token',
+    body('token').notEmpty().withMessage('El Token no puede ir vacío'),
+    handleInputErrors,
+    AuthController.validateToken
 )
 
 export default router
