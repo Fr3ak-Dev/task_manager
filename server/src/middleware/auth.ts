89b27e2b9e90c69894e539ad7a139f7ddd5b2c4b
gustavo <kg.2k.pl@gmail.com>
2025-08-01 20:48:42 -0400

    feat(auth): improve authentication middleware to verify the token

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,4 +1,6 @@
+import jwt from "jsonwebtoken"
 import { NextFunction, Request, Response } from "express"
+import User from "../models/User"
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization
@@ -7,7 +9,18 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
         return res.status(401).json({error: error.message})
     }
 
-    const [, token] = bearer.split(' ')
-    
+    const [, token] = bearer.split(' ') // Esto es para que el token sea el segundo elemento del array
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (typeof decoded === 'object' && decoded.id) {    // Verifica si el decoded es un objeto y tiene un id
+            const user = await User.findById(decoded.id)
+            console.log(user)
+        }
+    } catch (error) {
+        res.status(500).json({error: 'Token No Válido'})
+    }
+
     next()
 }
