48bf2600cf7666d9f13f5fd85f53df57b8de3299
gustavo <kg.2k.pl@gmail.com>
2025-08-01 20:57:11 -0400

    feat(auth): add user information to the request object after token verification

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,6 +1,14 @@
 import jwt from "jsonwebtoken"
 import { NextFunction, Request, Response } from "express"
-import User from "../models/User"
+import User, { IUser } from "../models/User"
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser
+        }
+    }
+}
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization
@@ -15,8 +23,12 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         if (typeof decoded === 'object' && decoded.id) {    // Verifica si el decoded es un objeto y tiene un id
-            const user = await User.findById(decoded.id)
-            console.log(user)
+            const user = await User.findById(decoded.id).select('_id name email')
+            if (user) {
+                req.user = user
+            } else {
+                res.status(500).json({error: 'Token No Válido'})
+            }
         }
     } catch (error) {
         res.status(500).json({error: 'Token No Válido'})
