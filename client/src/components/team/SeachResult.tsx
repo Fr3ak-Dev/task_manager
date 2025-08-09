3f359eae34a5624d52adb878a7b0fbbcc4c0cae8
gustavo <kg.2k.pl@gmail.com>
2025-08-08 20:26:36 -0400

    feat(TeamAPI, SeachResult): Add functionality to add users to a project

--- client/src/components/team/SeachResult.tsx
+++ client/src/components/team/SeachResult.tsx
@@ -1,10 +1,36 @@
+import { addUserToProject } from "@/api/TeamAPI"
 import { TeamMember } from "@/types/index"
+import { useMutation } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type SearchResultProps = {
     user: TeamMember
 }
 
 export default function SeachResult({user}: SearchResultProps) {
+
+  const params = useParams()
+  const projectId = params.projectId!
+
+  const {mutate} = useMutation({
+    mutationFn: addUserToProject,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+    }
+  })
+
+  const handleAddUserToProject = () => {
+    const data = {
+      projectId,
+      id: user._id
+    }
+    mutate(data)
+  }
+
   return (
     <>
         <p className="mt-10 text-center font-bold">Resultado:</p>
@@ -12,6 +38,7 @@ export default function SeachResult({user}: SearchResultProps) {
             <p>{user.name}</p>
             <button
                 className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold"
+                onClick={handleAddUserToProject}
             >Agregar al Proyecto</button>
         </div>
     </>
