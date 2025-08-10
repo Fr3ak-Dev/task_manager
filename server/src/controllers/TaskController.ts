a17df2a4e269bdca91e2d37bf9276dc05e83a7f0
gustavo <kg.2k.pl@gmail.com>
2025-08-09 20:56:55 -0400

    feat(task): update the structure of the 'completedBy' field to include user and status information

--- server/src/controllers/TaskController.ts
+++ server/src/controllers/TaskController.ts
@@ -59,15 +59,17 @@ export class TaskController {
         try {
             const { status } = req.body
             req.task.status = status
-            if (status === 'pending') {
-                req.task.completedBy = null
-            } else {
-                req.task.completedBy = req.user.id
+
+            const data = {
+                user: req.user.id,
+                status
             }
+            req.task.completedBy.push(data)
+
             await req.task.save()
-            res.send("Task status updated")
+            res.send("Estado de la tarea actualizado")
         } catch (error) {
-            res.status(500).json({ error: 'Internal server error' })
+            res.status(500).json({ error: 'Error Interno del Servidor' })
         }
     }
 }
