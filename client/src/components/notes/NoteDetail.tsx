33b897e0a4e09c634ebdfc5e740edc86d9016522
gustavo <kg.2k.pl@gmail.com>
2025-08-10 11:12:21 -0400

    feat(NoteDetail): add authorization logic to delete notes

--- client/src/components/notes/NoteDetail.tsx
+++ client/src/components/notes/NoteDetail.tsx
@@ -1,11 +1,19 @@
+import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMemo } from "react"
 
 type NoteDetailProps = {
     note: Note
 }
 
-export default function NoteDetail({note} : NoteDetailProps) {
+export default function NoteDetail({ note }: NoteDetailProps) {
+
+    const { data, isLoading } = useAuth()
+    const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+
+    if (isLoading) return 'Cargando...'
+
     return (
         <div className="p-3 flex justify-between items-center">
             <div>
@@ -14,6 +22,11 @@ export default function NoteDetail({note} : NoteDetailProps) {
                 </p>
                 <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
             </div>
+            {canDelete && (
+                <button type="button"
+                    className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+                >Eliminar</button>
+            )}
         </div>
     )
 }
