460e58e85cd64f805759572d4cd8a86579539103
gustavo <kg.2k.pl@gmail.com>
2025-08-12 06:20:54 -0400

    enhance(Task Card): add new styling to the Task Card component to simulate the actual layout

--- client/src/components/tasks/TaskCard.tsx
+++ client/src/components/tasks/TaskCard.tsx
@@ -36,7 +36,13 @@ export default function TaskCard({ task, canEdit }: TaskCardProps) {
   })
 
   const style = transform ? {
-    transform: `translate3d(${transform.x}px, (${transform.y}px, 0)`
+    transform: `translate3d(${transform.x}px, (${transform.y}px, 0)`,
+    padding: "1.25rem",
+    backgroundColor: '#FFF',
+    width: '300px',
+    display: 'flex',
+    borderWith: '1px',
+    borderColor: 'rgb(203 213 225 / var(--tw-border-opacity))'
   } : undefined
 
   return (
@@ -47,11 +53,7 @@ export default function TaskCard({ task, canEdit }: TaskCardProps) {
         ref={setNodeRef}
         style={style}
         className="min-w-0 flex flex-col gap-y-4">
-        <button type="button" className="text-xl font-bold text-slate-600 text-left"
-          onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
-        >
-          {task.name}
-        </button>
+        <p className="text-xl font-bold text-slate-600 text-left">{task.name}</p>
         <p className="text-slate-500">{task.description}</p>
       </div>
       <div className="flex shrink-0  gap-x-6">
